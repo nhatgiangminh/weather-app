0a3213b2840dd0eabf6dbd15cb08d8b1cf060108
nhatgiangminh <nhat.giang@smartyprocess.com>
2023-10-10 20:14:57 +0700

    change name

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -4,7 +4,7 @@
       <RouterLink :to="{ name: 'home' }">
         <div class="flex items-center gap-3 flex-1">
           <i class="fa-solid fa-sun text-2xl"></i>
-          <p class="text-2xl">The Local Weather</p>
+          <p class="text-2xl">Easy Weather</p>
         </div>
       </RouterLink>
       <div class="flex flex-1 justify-end gap-3">
